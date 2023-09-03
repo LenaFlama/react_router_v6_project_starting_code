80dbdbf4e8e2b64fd5b94f5281897f77e5715c6e
Lena <lena.flama@yahoo.com>
2023-09-03 20:31:39 +0300

    Creating nested routes and index route

--- starting_code/src/App.js
+++ starting_code/src/App.js
@@ -9,8 +9,12 @@ import { RouterProvider, createBrowserRouter,createRoutesFromElements, Route } f
 
 // create router with JSX Route elements
 const appRouter = createBrowserRouter(createRoutesFromElements(
-  <Route path='/root' element={<root></root>}></Route>
+  <Route path='/' element={<Root/>}>
+    <Route index element={<HomePage></HomePage>}></Route>
+  </Route>
 ));
+
+
 
 function App() {
   return (
